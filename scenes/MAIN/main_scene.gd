bcdf4570708ccbad15c714d42199ac47a291ec33
Martín Rovere <ma.rovere@gmail.com>
2025-01-30 00:53:37 -0300

    some scenes and deleted stuff

---
+++ scenes/MAIN/main_scene.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var rich_text_label = %RichTextLabel
+
+func _ready():
+	var json_data = Utils.load_json_data_from_path(Utils.PATH_DAYS_JSON_DATA)
+	if json_data != null:
+		rich_text_label.text = JSON.stringify(json_data)
